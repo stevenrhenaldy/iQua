a80cd9c1dd6c9043581f1f5937f2bbd2f236633c
stevenrhenaldy <stevenrhenaldy@gmail.com>
2023-07-07 02:50:42 +0700

    home controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                        @if (is_null(Auth::user()->email_verified_at))
+                            <div class="alert alert-warning" role="alert">
+                                <h4 class="alert-heading">Verify Your Email Address!</h4>
+                                <p>Before proceeding, please check your email for a verification link.</p>
+                                <hr class="my-2">
 
-                    {{ __('You are logged in!') }}
+                                <form class=" my-0 py-0" method="POST" action="{{ route('verification.resend') }}">
+                                    @csrf
+                                    <button type="submit" class="btn btn-success  m-0">{{ __('Resend') }}</button>.
+                                </form>
+                            </div>
+                        @endif
+
+                        {{-- {{ __('You are logged in!') }} --}}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/email/verify', function () {
+//     return view('auth.verify');
+// })->middleware('auth')->name('verification.notice');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.resend');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
