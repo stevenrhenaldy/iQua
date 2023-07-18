cbacf0d020c1f62415add162cc5a7a03eaedbf26
stevenrhenaldy <stevenrhenaldy@gmail.com>
2023-07-18 13:51:24 +0700

    Login Using Username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,49 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        $to_return = $this->guard()->attempt(
+            [
+                'username' => $request->username,
+                'password' => $request->password,
+            ],
+            $request->boolean('remember')
+        );
+        if (!$to_return) {
+            $to_return = $this->guard()->attempt(
+                [
+                    'email' => $request->username,
+                    'password' => $request->password,
+                ],
+                $request->boolean('remember')
+            );
+        }
+
+        return $to_return;
     }
 }
