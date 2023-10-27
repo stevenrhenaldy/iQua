8174a2d06143ed3bf006e2a97b4ac433e19a1213
stevenrhenaldy <stevenrhenaldy@gmail.com>
2023-10-27 20:35:58 +0800

    Navigation bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+    @stack("styles")
+
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
@@ -20,9 +22,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm py-1">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}" class="my-0 py-0">
+                <a class="navbar-brand" href="{{ url('/') }}" class="my-0 py-0" style="color: #395e7c">
                     <img src="{{asset("assets/logo/logo.svg")}}" alt="logo" style="height:37px" class="img img-responsive my-0" >
-                    {{ config('app.name', 'Laravel') }}
+                    <b>{{ config('app.name', 'Laravel') }}</b>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -31,20 +33,24 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+                        </li>
                         @auth
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
-                            </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('group.index') }}">{{ __('Groups') }}</a>
                             </li>
                         @endauth
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home') }}">{{ __('About Us') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
+
                             @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -85,5 +91,6 @@
             @yield('content')
         </main>
     </div>
+    @stack("js")
 </body>
 </html>
